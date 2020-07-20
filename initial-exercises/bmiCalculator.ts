03aa4a9868c966064781dbc9279c62b643b5e04d
Mei Sun 227 <meysunlee@gmail.com>
2020-07-20 13:19:03 +0300

    part 9.9

---
+++ initial-exercises/bmiCalculator.ts
@@ -0,0 +1,27 @@
+
+type Result = string;
+
+
+
+const calculateBmi = (weight: number, height: number): Result => {
+    const bmiValue = Math.round((weight / (height * height / 100) * 100));
+    if (bmiValue < 18.5) {
+        return `Light(Underweight)`;
+    }
+    if (bmiValue < 25) {
+        return `Normal (healthy weight)`;
+    } if (bmiValue > 25 && bmiValue < 40) {
+        return `Obesity class (Overweight)`;
+    }
+    return `malformatted parameters`;
+};
+
+const bmiCalculator = (weight: number, height: number): string => {
+    try {
+        return calculateBmi(weight, height);
+    } catch (e) {
+        return 'Something went wrong, error message: ${e.message}';
+    }
+
+};
+export default bmiCalculator;
